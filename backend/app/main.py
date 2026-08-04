45579edaf818efde1d09aa947396969b2a2b8e7a
Renan Albino Horne <199679478+RenanHornet@users.noreply.github.com>
2026-08-04 13:45:26 -0300

    feat: implementa primeira integração entre frontend e backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from app.routes.base import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="GabaritAI API",
     version="0.1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,1 +1,20 @@
 console.log("GabaritAI iniciado com sucesso!");
+
+async function testarAPI() {
+    try{ 
+        const response = await fetch('http://127.0.0.1:8000/');
+        const data = await response.json();
+
+        console.log(response);
+        console.log(data);
+        
+        if (data.status === 'API funcionando') {
+            document.getElementById('resultadoAPI').innerText = '🟢 Servidor Online';
+        } else {
+            document.getElementById('resultadoAPI').innerText = '🔴 Servidor Offline';
+        }
+    } catch (error) {
+        console.error('Erro ao testar a API:', error);
+        document.getElementById('resultadoAPI').innerText = '🔴 Servidor Offline';
+    }
+}
